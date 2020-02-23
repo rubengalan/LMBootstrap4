7298134614e5308b9bd059ddffa033af6f53951a
rubengalan <56016708+rubengalan@users.noreply.github.com>
2020-02-23 15:55:22 +0100

    Add files via upload

---
+++ js/misjs.js
@@ -0,0 +1,17 @@
+
+// Para verificar que la contraseña es la misma en los dos campos
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Las contraseñas no coinciden");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
